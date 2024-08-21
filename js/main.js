4da17c3f4a99c9a9735491950c448789f7505233
Lungdjos <lungudjos@gmail.com>
2024-08-21 12:22:47 +0200

    adding a js file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 //This is the main js page//
+'use strict'
+
+// declaring variables
+const nav_bar = document.querySelector('#navbar');
+const home = document.querySelector('#welcome-section');
+const projects = document.querySelector('#projects');
+const experience = document.querySelector('#work-experience-section');
